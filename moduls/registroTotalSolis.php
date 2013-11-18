1fcaafae2c479a97b992082b1c6742ba2870b39f
berseck <salvador.ml.lic@gmail.com>
2013-11-17 22:08:21 -0600

    modificacion al insert

--- moduls/registroTotalSolis.php
+++ moduls/registroTotalSolis.php
@@ -11,13 +11,13 @@ $atendio 	=$_SESSION['usuario'];
  include 'login.php';
  
 
- $rev="select * from historial where folio='$folio';";
+ $rev="select * from historial where idfactura='$folio';";
  $reves=pg_query($conn, $rev);
  if(pg_num_rows($reves)>0){
  	echo "Se Encunetra Registrado".$folio;
  }else{
 
- $regis="insert into historial(id_cl,nombre_cl,folio,fecha,doctor,total,pago,atendio)values($idcl,'$nombre_cli','$folio','$fecha','$doc',$costo_total,$cobro_total,'$atendio')";
+ $regis="insert into historial(id_clientes,idfactura,fecha,id_doc)values($idcl,'$folio','$fecha','$doc')";
  $entro=pg_query($conn, $regis);
  if(!$entro){
  	echo $regis;
